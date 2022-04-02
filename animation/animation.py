500c7b984eca792d705f6b7d266b976b2310468f
Francisco Quesada <fquesada@itba.edu.ar>
2022-04-02 16:48:33 -0300

    refactor :recycle: move logic to function

--- animation/animation.py
+++ animation/animation.py
@@ -1,52 +1,11 @@
 import argparse
 
-
-def calculate_cell_direction():
-    pass
-
-
-def write_static_lattice_file(out_filename, lattice_width, lattice_height):
-    # hexagonal lattice
-
-    static_lattice_file = open(out_filename, 'w')
-
-    static_lattice_file.write(f'{lattice_width*lattice_height}\n\n')
-
-    for y in range(lattice_height):
-        for x in range(lattice_width):
-
-            if y % 2 == 0:
-                x_offset = 0.5
-            else:
-                x_offset = 0
-
-            static_lattice_file.write(f'{x + x_offset}\t{y*0.866}\t0\n')
-
-    static_lattice_file.close()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=argparse.FileType('r'),
-                        default=None, help="File of lattice simulation timesteps", dest="lattice_steps", required=True)
-
-    args = parser.parse_args()
-
-    xyz_file = open("animation.xyz", "w")
-
-    # N
-    # comment
-    # xi yi 0
-
-    with args.lattice_steps as lattice_steps_file:
+def write_animation(out_filename,input_file):
+    xyz_file = open(out_filename, "w")
+    with input_file as lattice_steps_file:
         lines = lattice_steps_file.readlines()
         N = int(lines[0].split()[0])
         D = int(lines[1].split()[0])
-        lattice_height = int(lines[2].split()[0])
-        lattice_width = int(lines[2].split()[1])
-
-        write_static_lattice_file(
-            "static_lattice.xyz", lattice_width, lattice_height)
 
         lines = lines[3:]
 
@@ -71,5 +30,16 @@ if __name__ == '__main__':
                         x_offset = 0
 
                     xyz_file.write(f'{x + x_offset}\t{y*0.866}\t0\n')
-
     xyz_file.close()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=argparse.FileType('r'),
+                        default=None, help="File of lattice simulation timesteps", dest="lattice_steps", required=True)
+
+    args = parser.parse_args()
+
+    write_animation("animation.xyz",args.lattice_steps)
+
